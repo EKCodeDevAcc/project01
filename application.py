5c140ccf4b681976c7395f39a7fa4aec6b318d44
Edward Kang <edkang59@gmail.com>
2018-07-09 01:26:36 +0000

    login basic

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template, request, json
+from flask import Flask, session, render_template, request, redirect, json
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,23 +23,42 @@ db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
-def displayLogin():
+def login():
     return render_template("login.html")
 
-@app.route("/displaySignUp")
-def displaySignUp():
+@app.route("/loginPost", methods=['POST'])
+def loginPost():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if db.execute("SELECT * FROM members WHERE (username = :username) AND (password = :password)", {"username": username, "password": password}).rowcount != 1:
+        return render_template("error.html", message="Wrong username or password. Please try again")
+    else:
+        session['user'] = username
+        return redirect("index.html", message=username)
+
+@app.route("/signUp")
+def signUp():
     name_list = db.execute("SELECT * FROM members").fetchall()
     return render_template("signUp.html", name_list=name_list)
 
-@app.route("/sign", methods=['POST'])
-def sign():
+@app.route("/signUpPost", methods=['POST'])
+def signUpPost():
     username = request.form.get("username")
     password = request.form.get("password")
 
     if db.execute("SELECT * FROM members WHERE username = :username", {"username": username}).rowcount != 0:
-        return render_template("error.html", message="That username already exists. Please try different username")
+        return render_template("error.html", message="That username already exists. Please try different username", error_title="Sign Up")
 
     db.execute("INSERT INTO members (username, password) VALUES (:username, :password)", {"username": username, "password": password})
 
     db.commit()
     return render_template("success.html")
+
+@app.route("/index")
+def index():
+    username = session.get('user')
+    if session.get('user'):
+        return render_template('index.html', message=username)
+    else:
+        return render_template("error.html", message="Please login to access", error_title="Log In", error_button="/")
